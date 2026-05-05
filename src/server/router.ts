512fc15bf461dacd1b93185f82bda381b6393d57
fcarsi-droid <fcarsi@gmail.com>
2026-05-05 19:50:02 -0300

    Add files via upload

--- src/server/router.ts
+++ src/server/router.ts
@@ -5,6 +5,11 @@ import { moodRouter } from "./routers/mood";
 import { sleepRouter } from "./routers/sleep";
 import { hydrationRouter } from "./routers/hydration";
 import { caloriesRouter } from "./routers/calories";
+import { recipesRouter } from "./routers/recipes";
+import { wellnessRouter } from "./routers/wellness";
+import { communityRouter } from "./routers/community";
+import { culturalRouter } from "./routers/cultural";
+import { bookclubRouter } from "./routers/bookclub";
 
 export const appRouter = router({
   auth: authRouter,
@@ -13,6 +18,11 @@ export const appRouter = router({
   sleep: sleepRouter,
   hydration: hydrationRouter,
   calories: caloriesRouter,
+  recipes: recipesRouter,
+  wellness: wellnessRouter,
+  community: communityRouter,
+  cultural: culturalRouter,
+  bookclub: bookclubRouter,
 });
 
 export type AppRouter = typeof appRouter;
