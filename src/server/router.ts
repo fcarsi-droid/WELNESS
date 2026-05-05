f51c6750886463ddeb94db06266e828290557fb6
fcarsi-droid <fcarsi@gmail.com>
2026-05-05 19:05:17 -0300

    Add files via upload

--- src/server/router.ts
+++ src/server/router.ts
@@ -1,10 +1,18 @@
 import { router } from "./trpc";
 import { authRouter } from "./routers/auth";
 import { adminRouter } from "./routers/admin";
+import { moodRouter } from "./routers/mood";
+import { sleepRouter } from "./routers/sleep";
+import { hydrationRouter } from "./routers/hydration";
+import { caloriesRouter } from "./routers/calories";
 
 export const appRouter = router({
   auth: authRouter,
   admin: adminRouter,
+  mood: moodRouter,
+  sleep: sleepRouter,
+  hydration: hydrationRouter,
+  calories: caloriesRouter,
 });
 
 export type AppRouter = typeof appRouter;
